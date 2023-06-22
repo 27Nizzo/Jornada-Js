b109d778f998c63a5b7b9f833c45ebf258080b2b
Nizzo07 <afonso7557@gmail.com>
2023-06-22 17:32:33 +0100

    Ticket System

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log("The velocity of you car in this current position is ${vel}Km/h")
+
+if (vel > 50) {//Condição Simples
+    console.log("You got fined!");
+}
+
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'EUA'
+console.log("You are living in ${country}");
+if (country === "BRA") {
+    console.log('Brasilian');
+} else {
+    console.log('Foreign!! Arrest him immediatly');
+}
