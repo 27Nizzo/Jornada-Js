e455172e4e844281659fca21a4878af3183ade5a
Nizzo07 <afonso7557@gmail.com>
2023-06-23 09:52:13 +0100

    pequenas adaptações e um comentário para o futuro

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,5 +1,7 @@
 var hora = 7.30
 
+console.log(`São ${hora}h por isso... `);
+
 if (hora >=  7 && hora <= 11.59) {
     console.log('Bom Dia!!!');
 } else if (hora >= 12 && hora <= 19.59) {
@@ -7,3 +9,5 @@ if (hora >=  7 && hora <= 11.59) {
 } else {
     console.log('Boa Noite...');
 }
+
+//Podemos usar a função gethours() para nos fornecer as nossas horas atuais
