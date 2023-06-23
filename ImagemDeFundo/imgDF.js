65edc3300e5b773cf7e2075feffeba51d4726a62
Nizzo07 <afonso7557@gmail.com>
2023-06-23 16:25:56 +0100

    Teste de uma Imagem de fundo

---
+++ ImagemDeFundo/imgDF.js
@@ -0,0 +1,7 @@
+function carregarImagem() {
+    var myDiv = document.getElementById("myDiv");
+    var imagem = new img();
+    img.src = "c:/Users/afons/Downloads/IMG_0414.jpg"
+    myDiv.appendChild(img) 
+}
+carregarImagem();
