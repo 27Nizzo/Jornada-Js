12d6e9725971439b335fb81cc8209a93155b6a94
Nizzo07 <afonso7557@gmail.com>
2023-06-23 09:48:27 +0100

    Alguns exercicios simpoles de if's e else's

---
+++ aula12/ex011.js
@@ -0,0 +1,6 @@
+var idade = 22;
+if(idade < 18){
+    console.log('You are not allowed');
+} else {
+    console.log("you can enter");
+}
---
+++ aula12/ex012.js
@@ -0,0 +1,9 @@
+var hora = 7.30
+
+if (hora >=  7 && hora <= 11.59) {
+    console.log('Bom Dia!!!');
+} else if (hora >= 12 && hora <= 19.59) {
+    console.log('Boa Tarde!!!');
+} else {
+    console.log('Boa Noite...');
+}
