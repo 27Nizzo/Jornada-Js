b5c292b609a18bc2c00d1a13f44b07c5d91cb472
Nizzo07 <afonso7557@gmail.com>
2023-06-25 18:49:49 +0100

    pequenoSiteDeHoras

--- Exercicos/EX1.js
+++ Exercicos/EX1.js
@@ -1,1 +1,21 @@
-alert('Ola')
+
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var msg2 = window.document.getElementById('msg2')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        msg2.innerHTML = '<strong><em>BOM DIA</em></strong>'
+        document.body.style.backgroundColor = 'Grey'
+    } else if (hora >= 12 && hora < 20) {
+        msg2.innerHTML = '<strong><em>BOA TARDE!!</em></strong>'
+        document.body.style.background = 'Brown'
+    } else {
+        msg2.innerHTML = '<strong><em>BOA NOITE!!</em></strong>'
+        document.body.style.background = 'Blue'    
+    }
+
+}
