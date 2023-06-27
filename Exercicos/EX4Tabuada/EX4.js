e3c42fa2329ecdefbe22b4cb52c9274d6ae8a70a
Nizzo07 <afonso7557@gmail.com>
2023-06-27 10:46:06 +0100

    Inicio de Um site que te faz a tabuada ate 10

---
+++ Exercicos/EX4Tabuada/EX4.js
@@ -0,0 +1,9 @@
+function gerar() {
+    var num = document.getElementById('txtn')
+
+    if(num.value.length == 0) {
+        alert('Verifique os Seus Dados Novamente')
+    } else {
+        alert('Ok')
+    }
+}
