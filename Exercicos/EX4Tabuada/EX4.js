c435e0ba89a0c1207d8aeca424ffae1682c24f74
Nizzo07 <afonso7557@gmail.com>
2023-06-27 14:49:20 +0100

    Site Tabuada

--- Exercicos/EX4Tabuada/EX4.js
+++ Exercicos/EX4Tabuada/EX4.js
@@ -1,9 +1,17 @@
-function gerar() {
-    var num = document.getElementById('txtn')
 
-    if(num.value.length == 0) {
-        alert('Verifique os Seus Dados Novamente')
-    } else {
-        alert('Ok')
+    function gerar() {
+        var num = document.getElementById('txtn').value
+
+        if(num.length.value == 0) {
+            alert('Verifique os seus dados, e tente novamente')
+        } else {
+            var res = document.getElementById('res')
+            res.innerHTML = `A tabuada de ${num} é:<br> `
+            
+            var n = Number(num)
+            for(var i = 0; i <= 10; i++){
+                var result = n * i
+                res.innerHTML += `${n} x ${i} = ${result} <br>`
+            }
+        }
     }
-}
